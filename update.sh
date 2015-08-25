6f7e9ca4170f4957e0c537d214eb8adb2d5708bd
Anton Minin <anton.a.minin@gmail.com>
2015-08-25 19:21:32 +0300

    update repo

--- update.sh
+++ update.sh
@@ -1,16 +1,23 @@
-test $REPO != '' || REPO=`git remote -v | grep pushh | awk '{ print $2 }'`
-test $MESSAGE != '' || MESSAGE='update repo'
+test "$REPO" != '' || REPO=`git remote -v | grep push | awk '{ print $2 }'`
+test "$MESSAGE" != '' || MESSAGE='update repo'
 
-echo "=$REPO="
-echo "-$MESSAGE-"
+echo "REPO:    =$REPO="
+echo "MESSAGE: -$MESSAGE-"
 
 test -d web || (git clone $REPO -b gh-pages web)
+pushd web
+  git checkout gh-pages -f
+  git pull
+popd
 test -e composer.phar || curl -sS https://getcomposer.org/installer | php
 test -d satis || php composer.phar create-project composer/satis --stability=dev --keep-vcs
 php satis/bin/satis build satis.json web/
 
-git ci -a -m "$MESSAGE" && git push origin master
+if [[ "$1" == 'push' ]]; then
+  git commit -a -m "$MESSAGE" && git push origin master
 
-pushd web
-  git ci -a -m "$MESSAGE" && git push origin gh-pages
-popd
+  pushd web
+    git commit -a -m "$MESSAGE" && git push origin gh-pages
+  popd
+fi
+

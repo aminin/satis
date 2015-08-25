eb041addab96e9c9ae79cb3ca306822e7ba80719
Anton Minin <anton.a.minin@gmail.com>
2015-08-25 19:15:02 +0300

    update repo

--- update.sh
+++ update.sh
@@ -1,13 +1,16 @@
-test REPO != '' || REPO=git@github.com:aminin/satis.git
-test MESSAGE != '' || MESSAGE='update repo'
+test $REPO != '' || REPO=`git remote -v | grep pushh | awk '{ print $2 }'`
+test $MESSAGE != '' || MESSAGE='update repo'
+
+echo "=$REPO="
+echo "-$MESSAGE-"
 
 test -d web || (git clone $REPO -b gh-pages web)
 test -e composer.phar || curl -sS https://getcomposer.org/installer | php
 test -d satis || php composer.phar create-project composer/satis --stability=dev --keep-vcs
 php satis/bin/satis build satis.json web/
 
-git ci -a -m $MESSAGE && git push origin master
+git ci -a -m "$MESSAGE" && git push origin master
 
 pushd web
-  git ci -a -m $MESSAGE && git push origin gh-pages
+  git ci -a -m "$MESSAGE" && git push origin gh-pages
 popd
